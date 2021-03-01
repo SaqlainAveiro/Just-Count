a764287e455951cfe5de2822b980f08bb3fb9d9e
Md. Anwarul Habib <saqlainahs@gmail.com>
2021-03-01 18:31:08 +0600

    Add files via upload

---
+++ index.py
@@ -0,0 +1,57 @@
+from flask import Flask, redirect, url_for, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/about')
+def aboutpage():
+    return render_template("about.html")
+
+@app.route('/', defaults = {'name' : "0" , 'name1' : "0"})
+@app.route('/<name> <name1>')
+def mainpage(name,name1):
+    return render_template("index.html" , name = name, name1 = name1)
+
+
+@app.route('/submit' , methods = ['GET'])
+def submit():
+    if request.method =='GET':
+
+        s1 = request.args['ST']
+
+        x=len(s1)  
+
+        if x==0:
+            return redirect(url_for('mainpage' , name = "0" , name1 = "0"))
+
+        original_words = []
+
+        str0 = ""
+
+        for i in range(x):
+
+            if s1[i]=="'" or s1[i]=="-" or s1[i]=="_":
+                str0+=s1[i]
+                continue
+            
+            if i+1<x and ( s1[i+1]==" " or s1[i+1]=="," or s1[i+1]=="?" or s1[i+1]=="!" or s1[i+1]==".") or i+1==x:
+                if s1[i]>='A' and s1[i]<='Z' or s1[i]>='a' and s1[i]<='z' or s1[i]>='0' and s1[i]<='9':
+                    str0+=s1[i]
+                    original_words.append(str0)
+                    str0=""
+            else:
+                str0+=s1[i]
+        
+        if str0!="" and (str0!="-" or str0!=":" or str0!="`"):
+            original_words.append(str0)
+            
+        p=len(original_words)
+                    
+        return redirect(url_for('mainpage' , name = p , name1 = x))
+        
+    else: 
+        return redirect(url_for('mainpage'))
+
+
+
+if __name__=='__main__':
+    app.run(debug = True)
